df16196616129b5cd1d5533e525c276c8d4c5deb
Neelam Patidar <neelampatidar415@gmail.com>
2026-06-15 09:21:01 +0530

    handling deployment issues_00

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 async function register(req,res){
-    const { firstname , lastname, email, password } = req.body;
+    const { fullname , email, password } = req.body;
 
     const userExist = await userModel.findOne({email:email});
     if(userExist){
@@ -11,13 +11,18 @@ async function register(req,res){
     }
 
     const user = await userModel.create({
-        fullname:{firstname, lastname},
+        firstname: fullname.firstname,
+        lastname: fullname.lastname,
         email,
         password : await bcrypt.hash(password,10)
     });
     //token set karo
     const token = jwt.sign({_id:user._id},process.env.JWT_SECRET);
-    res.cookie("token",token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true, // REQUIRED on HTTPS (Render)
+      sameSite: "none", // safest option
+    });
 
     return res.status(201).json({message:"User created successfully"})
 } 
@@ -37,7 +42,11 @@ async function login(req,res){
     }
 
     const token = jwt.sign({_id:user._id},process.env.JWT_SECRET);
-    res.cookie("token",token);
+    res.cookie("token",token, {
+      httpOnly: true,
+      secure: true, // REQUIRED on HTTPS (Render)
+      sameSite: "none", // safest option
+    });
 
     return res.status(201).json({message:"logged in successfull"})
 } 
@@ -47,22 +56,20 @@ async function logout(req,res){
     return res.status(200).json({message:"Logged out successfully"});
 }
 
-async function checkAuth(req,res){
+async function checkAuth(req, res) {
+  try {
     const token = req.cookies.token;
-    if(!token){
-        return res.status(401).json({message:"Unauthorized"});
+
+    if (!token) {
+      return res.status(401).json({ message: "Not logged in" });
     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await userModel.findById(decoded._id);
-        if(!user){
-            return res.status(401).json({message:"Unauthorized"});
-        }
-        return res.status(200).json({user});
-    } catch (err) {
-        return res.status(401).json({message:"Unauthorized"});
-    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    res.json({ user: decoded });
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 }
 
 module.exports = {
