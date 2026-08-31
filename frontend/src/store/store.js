b9d706f27ad4552adf63ba5dd6eded777c3dfc8b
Neelam Patidar <neelampatidar415@gmail.com>
2026-08-31 12:00:48 +0530

    Fix chatSlice filename case

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import chatReducer from './chatSlice.js';
+import chatReducer from './chatSlice.js'; 
 
 export const store = configureStore({
     reducer: {
